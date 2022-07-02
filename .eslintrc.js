31c160721bb823023ba85090a799da2674f375da
satwik1608 <satwikkashyap1608@gmail.com>
2022-07-02 12:46:53 +0530

    done

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+// module.exports = {
+//   env: {
+//     node: 1,
+//   },
+//   rules: {
+//     'prettier/prettier': [
+//       'warn',
+//       {
+//         singleQuote: true,
+//         semi: true,
+//       }
+//     ],
+//   },
+// };
